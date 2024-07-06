0ac9c206ede0596b88bfd2732086ce330713065f
Rahul Sharma <rahulsharma@beatroute.io>
2024-07-06 20:25:32 +0530

    extend class

--- classes.js
+++ classes.js
@@ -1,1 +1,24 @@
-class test {}
+class User {
+    constructor(username) {
+        this.username = username;
+    }
+
+    logMe() {
+        console.log(this.username)
+    }
+}
+
+class Teacher extends User {
+    constructor(username,email) {
+        super();
+        this.username = username; 
+        this.email = email;
+    }
+
+    addCourse() {
+        console.log('course added by', this.username)
+    }
+}
+// const user = new User('teat');
+const teacher = new Teacher('teacher', 'teacher@mail.co')
+teacher.logMe();
