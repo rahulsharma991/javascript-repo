5049dc9c87569e87159620a703bad0df190e23e0
Rahul Sharma <rahulsharma@beatroute.io>
2024-07-17 23:27:22 +0530

    getter setter

--- classes.js
+++ classes.js
@@ -1,5 +1,6 @@
 class User {
-    static t = 0
+    static t = 0;
+    _poster = 'yes';
     constructor(username) {
         this.username = username;
     }
@@ -31,13 +32,22 @@ class Engineer extends Teacher {
     constructThings() {
         console.log('constructing')
     }
+    get email() {
+        return this._email;
+    }
+    set email(value) {
+        console.log(value)
+        this._email = value
+    }
 }
 // const user = new User('teat');
-// const teacher = new Teacher('teacher', 'teacher@mail.co')
+const teacher = new Teacher('teacher', 'teacher@mail.co')
 // teacher.logMe();
 // Teacher.creatId();
 // User.creatId();
 const engineer = new Engineer('engineer', 'engineerr@gmail.com');
-engineer.constructThings();
-engineer.logMe();
-engineer.addCourse();
+// engineer.constructThings();
+// engineer.logMe();
+// engineer.addCourse();
+// console.log(teacher._poster)
+console.log(engineer.email)
